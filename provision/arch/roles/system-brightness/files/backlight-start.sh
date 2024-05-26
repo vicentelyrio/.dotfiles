734f9bfd7c8acd1d55a9eb69d2658fdc912b142e
vicentelyrio <vicentelyrio@gmail.com>
2024-05-26 12:39:08 -0300

    layout improvements

--- provision/arch/roles/system-brightness/files/backlight-start.sh
+++ provision/arch/roles/system-brightness/files/backlight-start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-#if [ -z "$(ls -A /sys/class/backlight)" ]; then
-  MONITORADDRESS=$(ddcutil detect --terse --sleep-multiplier 0 | grep bus | awk -F '/' '{print $3}')
-  echo 'ddcci 0x37' | sudo tee /sys/bus/i2c/devices/$MONITORADDRESS/new_device
-#fi
+MONITORADDRESS=$(ddcutil detect --terse --sleep-multiplier 0 | grep bus | awk -F '/' '{print $3}')
+echo 'ddcci 0x37' | sudo tee /sys/bus/i2c/devices/$MONITORADDRESS/new_device
--- provision/arch/roles/ui-hyprland/files/hypr/scripts/autostart.sh
+++ provision/arch/roles/ui-hyprland/files/hypr/scripts/autostart.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-# Variables
-# config=$HOME/.config/hypr
-# scripts=$config/scripts
-
 # Automounter for removable media
 # udiskie &
-
-# Wallpaper Backend
-# swww-daemon --format xrgb &
-
-# Bar
-# waybar &
 
 # Plugins
 # pypr &
@@ -25,18 +15,15 @@
 # Notify about devices connecting and disconnecting
 # devify &
 
+# Interface
+$HOME/.config/ags/scripts/start.sh
+
 # Idle daemon to screen lock
 hypridle &
 
 # Wallpaper daemon
 hyprpaper &
 
-# Polkit authentication
-# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-
 # Screen sharing and portals
 # "$scripts"/portal &
 # xwaylandvideobridge &
-
-# Audio
-# easyeffects --gapplication-service &
