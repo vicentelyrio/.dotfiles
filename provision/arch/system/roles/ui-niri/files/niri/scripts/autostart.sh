b46b3d2bc2409438ea033fcf8c874ed77e270782
vicentelyrio <vicentelyrio@gmail.com>
2026-08-04 20:48:55 -0300

    feat(v2): font sizes

--- provision/arch/system/roles/ui-niri/files/niri/scripts/autostart.sh
+++ provision/arch/system/roles/ui-niri/files/niri/scripts/autostart.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Input method (needed for the ~/.XCompose cedilla override to apply to
+# GTK4/Wayland-native apps, e.g. Ghostty; GTK's old built-in "cedilla" IM
+# module never supported GTK4)
+fcitx5 -d --replace &
+
 # Logitech device manager
 solaar --window=hide &
 
@@ -13,7 +18,7 @@ clipse -listen &
 gsettings set org.gnome.desktop.interface gtk-theme "catppuccin-macchiato-lavender-standard+default"
 gsettings set org.gnome.desktop.interface cursor-theme "catppuccin-macchiato-dark-cursors"
 gsettings set org.gnome.desktop.interface cursor-size 24
-gsettings set org.gnome.desktop.interface font-name "JetBrainsMono Nerd Font 13"
+gsettings set org.gnome.desktop.interface font-name "JetBrainsMono Nerd Font 16"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 
 # Shell
